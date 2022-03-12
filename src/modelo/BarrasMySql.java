c725509f7eeb10ca6f9fbf85ebbf03a493bae97b
Angel Palacios Mirafuentes <al221910938@gmail.com>
2022-03-12 08:32:28 -0600

    clase 12 03 2022

---
+++ src/modelo/BarrasMySql.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.Barcode39;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Angel
+ */
+public class BarrasMySql {
+
+    public static void main(String[] args) throws SQLException, FileNotFoundException, DocumentException {
+        PreparedStatement ps = null;
+        ResultSet rs;
+        Connection con = null;
+        Image img;
+
+        Conexion conn = new Conexion();
+
+        try {
+            
+            ps = con.prepareStatement("SELECT * FROM articulos ");
+            
+            rs = ps.executeQuery();
+            
+            Document doc = new Document();
+            PdfWriter pdf = PdfWriter.getInstance(doc, new FileOutputStream("codigosSql.pdf"));
+            doc.open();
+
+            Barcode39 code = new Barcode39();
+
+            while (rs.next()) {
+                code.setCode(rs.getString("codigo"));
+                img = code.createImageWithBarcode(pdf.getDirectContent(), BaseColor.BLACK, BaseColor.BLACK);
+                img.scalePercent(120);
+                doc.add(img);
+                doc.add(new Paragraph(" "));
+
+            }
+            
+            doc.close();
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(BarrasMySql.class.getName()).log(Level.SEVERE, null,e);
+        }
+
+    }
+}
